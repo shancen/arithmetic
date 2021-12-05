7b86c8ac77685e3f6cbf42a541f35ed8ab9883a4
shanlll <shanlll@126.com>
2021-12-05 22:23:51 +0800

    第三周

---
+++ arithmetic/arithmetic/src/main/java/leetcode/editor/cn/week03/MergeKLists.java
@@ -0,0 +1,44 @@
+package leetcode.editor.cn.week03;
+
+
+// 这2题都不会做，参考的官方题解
+public class MergeKLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode ans = null;
+        for (int i = 0; i < lists.length; ++i) {
+            ans = mergeTwoLists(ans, lists[i]);
+        }
+        return ans;
+    }
+
+    public ListNode mergeTwoLists(ListNode a, ListNode b) {
+        if (a == null || b == null) {
+            return a != null ? a : b;
+        }
+        ListNode head = new ListNode(0);
+        ListNode tail = head, aPtr = a, bPtr = b;
+        while (aPtr != null && bPtr != null) {
+            if (aPtr.val < bPtr.val) {
+                tail.next = aPtr;
+                aPtr = aPtr.next;
+            } else {
+                tail.next = bPtr;
+                bPtr = bPtr.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = (aPtr != null ? aPtr : bPtr);
+        return head.next;
+    }
+     class ListNode {
+        int val;
+       ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+
+}
+
+
